f91adecaf0282919bb5693ceee98d83f42346d51
exercise1 <bhise@DESKTOP-VM6K5PC>
2020-02-07 12:08:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP_Inclass
+{
+    class Car
+    
+        {
+  public string model;  // Create a field
+        public string color;
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            model = "Mustang"; // Set the initial value for model
+            color = "Black";
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Ford.model);  // Print the value of model
+            Console.WriteLine(Ford.color);
+        }
+    }
+}
